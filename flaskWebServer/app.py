517507b130caf5fd513943dcfd7834a258362557
Lonk10 <luca0098@gmail.com>
2021-08-08 17:35:43 +0100

    updated for new backend

--- flaskWebServer/app.py
+++ flaskWebServer/app.py
@@ -8,33 +8,14 @@ import time
 import sys
 from backend import Backend
 
-
-
-print("Setup started")
-#HX711 setup
-
 app = Flask(__name__)
-"""
-scaleInt = HX711(5,6)
-referenceUnit = 390
-scaleInt.set_reading_format("MSB", "MSB")
-scaleInt.set_reference_unit(referenceUnit)
-scaleInt.reset()
-scaleInt.tare()
-"""
-
-#Servo setup
-#GPIO.setmode(GPIO.BOARD) # Set GPIO numbering mode
-#GPIO.setup(18,GPIO.OUT) # Set pin 37 as an output, and set servo as pin 37 as PWM
-#servo = GPIO.PWM(18,50) # Note 11 is pin, 50 = 50Hz pulse
-print("Setup finished")
 
 @app.route("/")
 def index():
 	# Read Sensors Status
       scaleSts = bk.getWeight()
       templateData = {
-            'title' : 'GPIO input Status!',
+            'title' : 'Cat food dispenser!',
             'scale'  : scaleSts,
       }
       return render_template('index.html', **templateData)
@@ -50,8 +31,8 @@ def dispenseWeight(weight=20):
       return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
+      bk = Backend()
       try: 
-            bk = Backend()
             app.run(host='0.0.0.0', port=80, debug=True)
       finally:
             bk.cleanAndExit()
