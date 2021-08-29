20b9f5259c00a1f3e049606306a2e9a647f1c9d9
Lonk10 <luca0098@gmail.com>
2021-08-29 18:06:49 +0200

    Added ability to delete an entry

--- flaskWebServer/scheduler.py
+++ flaskWebServer/scheduler.py
@@ -14,9 +14,41 @@ class Config:
     def set_key(self, key, attr):
         self.conf_dict[key].append(attr)
 
+    def del_entry(self, hKey, wKey):
+        index = 0
+        for item in self.get_key("orari"):
+            #check if both elems are correct
+            if item == hKey and self.conf_dict["grammi"][index] == wKey :
+                break
+            index = index + 1
+        #delete elems from dict
+        del self.conf_dict["orari"][index]
+        del self.conf_dict["grammi"][index]
+
+        #update json
+        with open("scheduler.json", "w") as file:
+            json.dump(self.conf_dict, file)
+
+    def dictZip(self):
+        orari = self.get_key("orari")
+        wgts = self.get_key("grammi")
+        return list(zip(orari, wgts))
+
+    def sortJson(self):
+        #sorts json data
+        zipped = self.dictZip()
+        ziplist = sorted(zipped, key=lambda tup: (tup[0]))
+        new_dict = {"orari": [], "grammi": [] }
+        for item in ziplist:
+                new_dict["orari"].append(item[0])
+                new_dict["grammi"].append(item[1])
+        self.conf_dict = new_dict
+        #updates global variable
+
     def set_entry(self, schedule, weight):
         self.set_key("orari", schedule)
         self.set_key("grammi", weight)
+        self.sortJson()
         with open("scheduler.json", "w") as file:
             json.dump(self.conf_dict, file)
 
